2ea361418e624e0f3c352abecd305250f8c2ac88
@RubenKelevra <ruben@freifunk-nrw.de>
2015-06-21 14:52:33 +0200

    initial structure of script written in debug-messages

--- node-scripts/make-backup.sh
+++ node-scripts/make-backup.sh
@@ -1,1 +1,11 @@
-#
+#!/bin/bash
+
+echo "checking preconditions..."
+
+echo "running a full systembackup on the cold-storage-node. Informing control-vm...."
+
+echo "backup-job done. Informing control-vm..."
+
+echo "backup successful completed, writing status-info to local systemlog..."
+
+echo "done."
