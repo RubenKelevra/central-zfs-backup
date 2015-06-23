32da88b0d4759cc1b2ffb75b65dc4e6d7310752e
@RubenKelevra <ruben@freifunk-nrw.de>
2015-06-23 14:11:27 +0200

    fix ssh syntax

--- node-scripts/make-backup.sh
+++ node-scripts/make-backup.sh
@@ -10,7 +10,7 @@ if test $? -ne 0; then
 fi
 
 echo "we need the cold-storage-node. Informing control-vm...."
-ssh $CONTROL_NODE_SSHUSER@$CONTROL_NODE_HOSTNAME:$CONTROL_NODE_SSHPORT $SSH_OPTIONS "bash /usr/local/bin/start-cold-storage-node.sh"
+ssh $CONTROL_NODE_SSHUSER@$CONTROL_NODE_HOSTNAME -p$CONTROL_NODE_SSHPORT $SSH_OPTIONS "bash /usr/local/bin/start-cold-storage-node.sh"
 if test $? -ne 0; then
 	echo "failed."
 	logger "can't inform control-node, that want to start the cold-storage..."
@@ -24,7 +24,7 @@ STORAGE_NODE_TRIES=0
 while [ $STORAGE_NODE_TRIES -le 4 ]; do
 	STORAGE_NODE_TRIES=$( expr $STORAGE_NODE_TRIES + 1 )
 	echo "try to reach storage-node...."
-	ssh $STORAGE_NODE_SSHUSER@$STORAGE_NODE_HOSTNAME:$STORAGE_NODE_SSHPORT $SSH_OPTIONS "exit"
+	ssh $STORAGE_NODE_SSHUSER@$STORAGE_NODE_HOSTNAME -p$STORAGE_NODE_SSHPORT $SSH_OPTIONS "exit"
 	if test $? -ne 0; then
 		echo "failed, retrying in $( expr $STORAGE_NODE_BOOTTIME / 10 )"
 		sleep $( expr $STORAGE_NODE_BOOTTIME / 10 )
@@ -40,7 +40,7 @@ if [ $STORAGE_NODE_TRIES -eq 5 ]; then
 fi
 
 echo "want to run a full systembackup on the cold-storage-node. Informing control-vm...."
-ssh $CONTROL_NODE_SSHUSER@$CONTROL_NODE_HOSTNAME:$CONTROL_NODE_SSHPORT $SSH_OPTIONS "touch $CONTROL_NODE_NOTIFYFOLDER/$HOSTNAME-started-backup"
+ssh $CONTROL_NODE_SSHUSER@$CONTROL_NODE_HOSTNAME -p$CONTROL_NODE_SSHPORT $SSH_OPTIONS "touch $CONTROL_NODE_NOTIFYFOLDER/$HOSTNAME-started-backup"
 if test $? -ne 0; then
 	echo "failed."
 	logger "can't inform control-node, that want to start the backup..."
@@ -52,12 +52,12 @@ rsync -qaAXve "ssh -p$STORAGE_NODE_SSHPORT" --exclude={"/dev/*","/proc/*","/sys/
 
 if test $? -ne 0; then
 	echo "backup-job failed, informing control-vm..."
-	ssh $CONTROL_NODE_SSHUSER@$CONTROL_NODE_HOSTNAME:$CONTROL_NODE_SSHPORT $SSH_OPTIONS "echo \"failed\" > $CONTROL_NODE_NOTIFYFOLDER/$HOSTNAME-finished-backup" #FIXME
+	ssh $CONTROL_NODE_SSHUSER@$CONTROL_NODE_HOSTNAME -p$CONTROL_NODE_SSHPORT $SSH_OPTIONS "echo \"failed\" > $CONTROL_NODE_NOTIFYFOLDER/$HOSTNAME-finished-backup" #FIXME
 	logger "root-backup failed, rsync returned an error..."
 	exit 1
 else
 	echo "backup-job done. Informing control-vm..."
-	ssh $CONTROL_NODE_SSHUSER@$CONTROL_NODE_HOSTNAME:$CONTROL_NODE_SSHPORT $SSH_OPTIONS "echo \"$BACKUP_TIME\" > $CONTROL_NODE_NOTIFYFOLDER/$HOSTNAME-finished-backup" #FIXME
+	ssh $CONTROL_NODE_SSHUSER@$CONTROL_NODE_HOSTNAME -p$CONTROL_NODE_SSHPORT $SSH_OPTIONS "echo \"$BACKUP_TIME\" > $CONTROL_NODE_NOTIFYFOLDER/$HOSTNAME-finished-backup" #FIXME
 	if test $? -ne 0; then
 		echo "failed."
 		logger "can't inform storage-node, that the backup was completed..."
